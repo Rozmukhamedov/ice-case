3a22880d8bd38f5d3534c4a57ae0291cee610995
Rozmukhamedov Islom <workislom@gmail.com>
2022-11-12 17:14:47 +0500

    updaate

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -2,9 +2,12 @@ import { Container, Grid } from "@mantine/core";
 import { t } from "i18next";
 import React from "react";
 import IMG from "assets/img/mobile2.png";
-import "./style.css"
+import "./style.css";
+import { useTranslation } from "react-i18next";
 
 function Order() {
+  const { t } = useTranslation();
+
   return (
     <>
       <Container>
@@ -12,27 +15,31 @@ function Order() {
           <div className="order__box">
             <Grid>
               <Grid.Col span={12} md={6} lg={6} xl={6}>
-                <h3>Возможности</h3>
-                <p>Отображение и хранение NFT</p>
-                <p>Отображение баланса криптовалют</p>
-                <p>Подключение к сети интернет при помощи Wi-Fi модуля</p>
-                <p>Сканирование QR-кода</p>
-                <p>
-                  Защита от внешних факторов и автономная работа с аккумулятором
-                </p>
+                <h3>{t("order_desc_1")}</h3>
+                <p>{t("order_desc_2")}</p>
+                <p>{t("order_desc_3")}</p>
+                <p>{t("order_desc_4")}</p>
+                <p>{t("order_desc_5")}</p>
+                <p>{t("order_desc_6")}</p>
               </Grid.Col>
-              <Grid.Col span={12} md={6} lg={6} xl={6} className={"order__box-right"}>
-                <h3>Комплектация</h3>
-                <p>Камера для сканирования QR-кода</p>
-                <p>Аккумулятор</p>
+              <Grid.Col
+                span={12}
+                md={6}
+                lg={6}
+                xl={6}
+                className={"order__box-right"}
+              >
+                <h3>{t("order_desc_7")}</h3>
+                <p>{t("order_desc_8")}</p>
+                <p>{t("order_desc_9")}</p>
                 <img src={IMG} alt="" />
               </Grid.Col>
             </Grid>
           </div>
         </div>
         <div className="order__show">
-          <h2>Более 547 предзаказов</h2>
-          <h5>получено на кошельки IceCase</h5>
+          <h2>{t("order_desc_10")}</h2>
+          <h5>{t("order_desc_11")}</h5>
         </div>
       </Container>
     </>
--- src/components/crypto/Crypto.tsx
+++ src/components/crypto/Crypto.tsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Container, Grid } from "@mantine/core";
 import Image from "assets/img/cripto.png";
 import "./style.css"
+import { useTranslation } from "react-i18next";
 
 function Crypto() {
+  const { t } = useTranslation();
+
   return (
     <div className="crypto">
       <Container>
         <div className="crypto__box">
           <Grid>
             <Grid.Col span={12} md={7} lg={7} xl={7}>
-              <h3>Сотни <br /> криптовалют</h3>
+              <h3>{t("crypto_1")}</h3>
               <p>
-              Аппаратный кошелек <br /> для вашего Биткойна, <br /> Эфира и сотен других <br /> криптовалют одновременно.
+             {t("crypto_2")}
               </p>
-              <h6>Все в одном кошельке!</h6>
+              <h6>{t("crypto_3")}</h6>
             </Grid.Col>
             <Grid.Col span={12} md={5} lg={5} xl={5}>
               <div className="crypto__box-image">
